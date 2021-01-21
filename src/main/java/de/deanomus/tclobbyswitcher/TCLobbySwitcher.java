828b5afcbbb9bf0f7af46eb64618ee367d464e85
Dean Reinhardt <dean@reinhardt.work>
2021-01-21 12:57:53 +0100

    small changes, removed tests/debugs outputs

--- src/main/java/de/deanomus/tclobbyswitcher/TCLobbySwitcher.java
+++ src/main/java/de/deanomus/tclobbyswitcher/TCLobbySwitcher.java
@@ -12,15 +12,15 @@ import org.bukkit.plugin.java.annotation.plugin.author.Author;
  * set all required parameters for the plugin.yml
  * with this annotations
  */
-@Plugin (name="TCLobbySwitcher", version="3.0")
-@Description (value="An TimoCloud Addon which adds an LobbySwitcher")
-@Author (value="deanomus")
+@Plugin(name="TCLobbySwitcher", version="3.0")
+@Description(value="An TimoCloud Addon which adds an LobbySwitcher")
+@Author(value="deanomus")
 //@Dependency (value="TimoCloud")
 public class TCLobbySwitcher extends JavaPlugin {
 
 
 
-    public void onEnable(Integer id) {
+    public void onEnable() {
         Logger log = Bukkit.getLogger();
 
         log.info("$aIt's starting o.O");
