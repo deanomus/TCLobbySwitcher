ce191f737c691eb0d92fa3de4f28f6a5a4ac9491
Dean Reinhardt <dean@reinhardt.work>
2021-01-21 12:28:21 +0100

    fix all sider review issues hopefuly

--- src/main/java/de/deanomus/tclobbyswitcher/TCLobbySwitcher.java
+++ src/main/java/de/deanomus/tclobbyswitcher/TCLobbySwitcher.java
@@ -1,35 +1,34 @@
 package de.deanomus.tclobbyswitcher;
 
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
-import java.util.logging.Logger;
-
 /**
  * Main Class of the plugin
- * set all required paramters for the plugin.yml
+ * set all required parameters for the plugin.yml
  * with this annotations
  */
-@Plugin(name="TCLobbySwitcher", version="3.0")
-@Description(value="An TimoCloud Addon which adds an LobbySwitcher")
-@Author(value="deanomus")
-//@Dependency(value="TimoCloud")
+@Plugin (name="TCLobbySwitcher", version="3.0")
+@Description (value="An TimoCloud Addon which adds an LobbySwitcher")
+@Author (value="deanomus")
+//@Dependency (value="TimoCloud")
 public class TCLobbySwitcher extends JavaPlugin {
 
 
-    @Override
-    public void onEnable() {
+
+    public void onEnable(Integer id) {
         Logger log = Bukkit.getLogger();
 
         log.info("$aIt's starting o.O");
     }
 
 
-    @Override
     public void onDisable() {
+
     }
 
 
