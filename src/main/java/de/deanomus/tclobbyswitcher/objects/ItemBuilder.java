291e6c89b24bd66ebef92b25183e38b24687f4ac
Dean Reinhardt <dean@reinhardt.work>
2021-01-24 20:51:54 +0100

    ItemBuilder.java javadoc

--- src/main/java/de/deanomus/tclobbyswitcher/objects/ItemBuilder.java
+++ src/main/java/de/deanomus/tclobbyswitcher/objects/ItemBuilder.java
@@ -55,6 +55,11 @@ public class ItemBuilder {
         return item;
     }
 
+    /**
+     * Add loretext to lores array
+     * @param text
+     * @return ItemBuilder
+     */
     public ItemBuilder addLoreText(String text) {
         if (this.lores == null) {
             this.lores = new ArrayList<String>();
@@ -63,6 +68,11 @@ public class ItemBuilder {
         return this;
     }
 
+    /**
+     * Add list of lorelexts to lores array
+     * @param text
+     * @return ItemBuilder
+     */
     public ItemBuilder addLoreText(String[] text) {
         for (String s : text) {
             addLoreText(s);
@@ -74,46 +84,92 @@ public class ItemBuilder {
 
     // GETTER & SETTER
 
+    /**
+     * Returns the material
+     *
+     * @return Material
+     */
     public Material getMaterial() {
         return material;
     }
 
+    /**
+     * Sets the material
+     * @param material
+     * @return ItemBuilder
+     */
     public ItemBuilder setMaterial(Material material) {
         this.material = material;
         return this;
     }
 
+    /**
+     * Returns the subId of material
+     * @return subId
+     */
     public short getSubId() {
         return subId;
     }
 
+    /**
+     * Sets the subId
+     * @param subId
+     * @return ItemBuilder
+     */
     public ItemBuilder setSubId(short subId) {
         this.subId = subId;
         return this;
     }
 
+    /**
+     * Returns the amount
+     * @return amount
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Sets the amount
+     * @param amount
+     * @return ItemBuilder
+     */
     public ItemBuilder setAmount(int amount) {
         this.amount = amount;
         return this;
     }
 
+    /**
+     * Returns the (display-)name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the (display-)name
+     * @param name
+     * @return ItemBuilder
+     */
     public ItemBuilder setName(String name) {
         this.name = name;
         return this;
     }
 
+    /**
+     * Returns the lores as list
+     * @return lores
+     */
     public ArrayList<String> getLores() {
         return lores;
     }
 
+    /**
+     * Sets the lores
+     * @param lores
+     * @return ItemBuilder
+     */
     public ItemBuilder setLores(ArrayList<String> lores) {
         this.lores = lores;
         return this;
